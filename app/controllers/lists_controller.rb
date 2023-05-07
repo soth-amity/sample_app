8daf9f7c3905c8062a3273100e94d522a8dc029e
soth-amity <130268293+soth-amity@users.noreply.github.com>
2023-05-07 00:58:06 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,16 @@
 class ListsController < ApplicationController
   def new
      # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @list = List.new 
+    @list = List.new
   end
-  
+
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -18,15 +18,16 @@ class ListsController < ApplicationController
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
   end
-  
+
   private
   # ストロングパラメータ
   def list_params
     params.require(:list).permit(:title, :body)
   end
-  
+
 end
